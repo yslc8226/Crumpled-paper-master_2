a09afddc9b717191a14b22ab8d6f6c5051ec060f
yslc8226 <75553336+yslc8226@users.noreply.github.com>
2021-01-09 14:02:35 +0800

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,62 @@
+class dustbin
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=200;
+		this.dustbinHeight=210;
+		this.wallThickness=20;
+		this.angle=0;	
+		
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+        this.image = loadImage("dustbingreen.png");
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			//rectMode(this.body.position.x, this.body.position.y, 50, 50);
+
+			push()
+			translate(posLeft.x, posLeft.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			stroke(255)
+			rotate(this.angle)
+			//image(this.image,0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+          
+			push()
+			translate(posRight.x, posRight.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			stroke(255)
+			angleMode(RADIANS)
+			fill(255)
+			rotate(-1*this.angle)
+			//image(this.image,0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			stroke(255)
+			angleMode(RADIANS)
+			fill(255)
+			image(this.image,0,-this.dustbinHeight/2,this.dustbinWidth,this.dustbinHeight);
+			pop()
+			
+	}
+
+}
